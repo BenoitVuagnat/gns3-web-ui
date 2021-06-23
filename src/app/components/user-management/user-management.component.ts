a825fa9ac752d2d39663406c03940bd85a7d9b5c
BenoitVuagnat <benoit.vuagnat@orange.com>
2021-06-23 16:32:58 +0200

    started the feature that shows all users

--- src/app/components/user-management/user-management.component.ts
+++ src/app/components/user-management/user-management.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Server } from '../../models/server';
+import { ServerService } from '../../services/server.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-management',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserManagementComponent implements OnInit {
 
-  constructor() { }
+  users:any[]=[];
+  public server: Server;
+
+  constructor(
+    private userService:UserService,
+    private route: ActivatedRoute,
+    private serverService: ServerService
+    ) {}
 
   ngOnInit() {
+    let serverId = this.route.snapshot.paramMap.get('server_id');
+        this.serverService.get(+serverId).then((server: Server) => {
+            this.server = server;
+            this.userService.getUser(server).subscribe((response: any) => {
+              this.users = response;
+          });
+        });
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,8 +9,14 @@ export class UserService {
   constructor(
     private httpServer: HttpServer
   ) {}
+  
 
   getInformationAboutLoggedUser(server: Server) {
     return this.httpServer.get<User>(server, '/users/me/');
+  }
+
+  getUser(server: Server){
+    //return this.http.get("http://10.237.0.165:3080/v3/users");
+    return this.httpServer.get(server, 'users');
   }
 }
